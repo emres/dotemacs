9486fe66d7e017ed10e9dd38f3d7386def8c8001
Emre Sevinç <emre.sevinc@gmail.com>
2018-10-22 10:40:22 +0200

    Add yafolding configuration
    Add yafolding configuration for code folding.

--- .emacs.el
+++ .emacs.el
@@ -412,3 +412,19 @@
 (setq highlight-indent-guides-responsive 'stack)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; For documentation, see https://github.com/zenozeng/yafolding.el
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defvar yafolding-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "<C-S-return>") #'yafolding-hide-parent-element)
+    (define-key map (kbd "<C-M-return>") #'yafolding-toggle-all)
+    (define-key map (kbd "<C-return>") #'yafolding-toggle-element)
+    map))
+
+(global-set-key (kbd "s-d y") 'yafolding-discover)
+
+(add-hook 'prog-mode-hook
+          (lambda () (yafolding-mode)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
