bbff6f790124b522d80abccfcdb78b3d6395556f
Emre Sevinç <emre.sevinc@gmail.com>
2026-08-15 19:03:12 +0200

    Modernize use-package bootstrap
    Fixes #36

--- .emacs.el
+++ .emacs.el
@@ -157,16 +157,13 @@ see: https://emacs.stackexchange.com/a/55166/8887"
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Set up use-package
-;;
-;; This is only needed once, near the top of the file
+;; Install use-package if missing
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
-(eval-when-compile
-  ;; Following line is not needed if use-package.el is in ~/.emacs.d
-  ;;(add-to-list 'load-path "<path where use-package is installed>")
-  (require 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
